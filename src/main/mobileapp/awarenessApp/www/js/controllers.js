3ae9db7c73bf0f014762d149aec526f9d6f1583d
poolebu <poolebu@gmail.com>
2015-06-19 08:14:55 -0500

    Adds first counter interface

--- src/main/mobileapp/awarenessApp/www/js/controllers.js
+++ src/main/mobileapp/awarenessApp/www/js/controllers.js
@@ -1,20 +1,53 @@
+function Entry (date, counter){
+
+          // Add object properties like this
+          this.date = date;
+          this.counter = counter;
+
+       };
+
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+    .controller('ChatsCtrl', function($scope, Chats) {
+        $scope.chats = Chats.all();
+        $scope.remove = function(chat) {
+            Chats.remove(chat);
+        }
+    })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+        $scope.counter= 3;
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+
+
+        $scope.historyArray = [];
+
+        $scope.addToCounter = function(){
+
+
+
+            $scope.counter = $scope.counter +1;
+
+            var entry = new Entry(new Date(),$scope.counter);
+            console.log(entry);
+            $scope.historyArray.push(entry);
+
+        };
+
+        $scope.substractToCounter = function(){
+
+            if ($scope.counter>0)
+                $scope.counter = $scope.counter -1;
+
+        }
+
+    })
+
+    .controller('AccountCtrl', function($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
+    });
